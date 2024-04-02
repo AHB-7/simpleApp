5903980a78fc8242a3fd06ed53dfd7a950536525
AHB7 <allanbtim7@gmail.com>
2024-04-02 14:37:50 +0200

    unit testing is satup

--- testing.js
+++ testing.js
@@ -1,4 +1,9 @@
-export function add(a, b) {
-    const result = a + b;
-    return result;
+export async function complexRemoteCall() {
+    const endpoints = [
+        fetch("https://a.api.com"),
+        fetch("https://b.api.com"),
+        fetch("https://c.api.com"),
+    ];
+
+    return await Promise.all(endpoints);
 }
--- testing.test.js
+++ testing.test.js
@@ -1,5 +1,23 @@
-import { add } from "./testing.js";
+import { complexRemoteCall } from "./testing.js";
 
-test("add", () => {
-    expect(add(2, 3)).toBe(5);
+beforeEach(() => {
+    global.fetch = jest.fn((url) =>
+        Promise.resolve({
+            json: () => Promise.resolve({ mockResponse: true }),
+        })
+    );
+});
+
+describe("complexRemoteCall", () => {
+    it("should return an array of responses from three API endpoints", async () => {
+        await complexRemoteCall();
+        expect(global.fetch).toHaveBeenCalledTimes(3);
+    });
+
+    it("should make an API request to each of the API URLs", async () => {
+        await complexRemoteCall();
+        expect(global.fetch).toHaveBeenNthCalledWith(1, "https://a.api.com");
+        expect(global.fetch).toHaveBeenNthCalledWith(2, "https://b.api.com");
+        expect(global.fetch).toHaveBeenNthCalledWith(3, "https://c.api.com");
+    });
 });
